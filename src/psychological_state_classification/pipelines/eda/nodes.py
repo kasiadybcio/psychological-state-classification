ce1c7a7c04bad227954c9dbafba7f713f36acdd2
Katarzyna Dybcio <kasiadybcio7@gmail.com>
2025-01-02 16:53:26 +0100

    plots eda2

--- src/psychological_state_classification/pipelines/eda/nodes.py
+++ src/psychological_state_classification/pipelines/eda/nodes.py
@@ -2,25 +2,66 @@
 This is a boilerplate pipeline 'eda'
 generated using Kedro 0.19.10
 """
-import seaborn as sns
-import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objs as go
 import pandas as pd
 
-def plot_missing(data :pd.DataFrame):
-    sns.set_style("whitegrid",{'font_scale':'1.6'})
-    plt.figure(figsize=(10, 6))
-    plt.title("Missing data per column %")
-    sns.barplot(y = data.isna().sum()/data.shape[0],
-                x = data.columns)
-    plt.ylim(0,1)
-    plt.xticks(rotation=90)
-    return plt
+def plot_mood_state_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'mood'
+    """
+    return (data.groupby('Mood_State')
+            .count()[['Time']]
+            .reset_index()
+            .rename(columns={'Time':'Count'}))
 
-def plot_dist_hrv(data :pd.DataFrame):
-    sns.set_style("whitegrid",{'font_scale':'1.6'})
-    plt.figure(figsize=(10, 6))
-    sns.histplot(data['HRV'])
-    plt.title('Distribution of Heart Rate Variability')
-    plt.xlabel('HRV')
-    plt.ylabel('Count')
-    return plt
+def plot_HRV_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'HRV'
+    """
+    return data[['HRV']]
+
+def plot_GSR_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'GSR'
+    """
+    return data[['GSR']]
+
+def plot_Age_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'Age'
+    """
+    return data[['Age']]
+
+def plot_Heart_Rate_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'Heart_Rate'
+    """
+    return data[['Heart_Rate']]
+
+def plot_Gender_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'Gender'
+    """
+    return (data.groupby('Gender')
+            .count()[['Time']]
+            .reset_index()
+            .rename(columns={'Time':'Count2'}))
+
+def plot_Task_Type_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'Task_Type'
+    """
+    return (data.groupby('Task_Type')
+            .count()[['Time']]
+            .reset_index()
+            .rename(columns={'Time':'Count3'}))
+
+def plot_Educational_Level_distribution(data: pd.DataFrame):
+    """
+    Plot the distribution of the target variable 'Educational_Level'
+    """
+    return (data.groupby('Educational_Level')
+            .count()[['Time']]
+            .reset_index()
+            .rename(columns={'Time':'Count4'}))
--- src/psychological_state_classification/pipelines/eda/pipeline.py
+++ src/psychological_state_classification/pipelines/eda/pipeline.py
@@ -6,19 +6,70 @@ generated using Kedro 0.19.10
 from kedro.pipeline import Pipeline, pipeline, node
 from .nodes import *
 
-
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
-            func=plot_missing,
-            inputs="raw_data",
-            outputs="missing_data_plot",
-            name="plot_missing_node"
-        ),
-        node(
-            func=plot_dist_hrv,
+            plot_mood_state_distribution,
             inputs="preprocessed_data",
-            outputs="dist_hrv_plot",
-            name="plot_hrv_distribution"
+            outputs="plot_mood_state_distribution_px",
+            name="plot_mood_state_distribution",
+            namespace='plotting'
+        ),
+
+        node(
+            plot_HRV_distribution,
+            inputs="preprocessed_data",
+            outputs="plot_HRV_distribution_px",
+            name="plot_HRV_distribution",
+            namespace='plotting'
+        ),
+
+         node(
+            plot_GSR_distribution,
+            inputs="preprocessed_data",
+            outputs="plot_GSR_distribution_px",
+            name="plot_GSR_distribution",
+            namespace='plotting'
+        ),
+
+        node(
+            plot_Age_distribution,
+            inputs="preprocessed_data",
+            outputs="plot_Age_distribution_px",
+            name="plot_Age_distribution",
+            namespace='plotting'
+        ),
+
+        node(
+            plot_Heart_Rate_distribution,
+            inputs="preprocessed_data",
+            outputs="plot_Heart_Rate_distribution_px",
+            name="plot_Heart_Rate_distribution",
+            namespace='plotting'
+        ),
+
+        node(
+            plot_Gender_distribution,
+            inputs="preprocessed_data",
+            outputs="plot_Gender_distribution_px",
+            name="plot_Gender_distribution",
+            namespace='plotting'
+        ),
+
+        node(
+            plot_Task_Type_distribution,
+            inputs="preprocessed_data",
+            outputs="plot_Task_Type_distribution_px",
+            name="plot_Task_Type_distribution",
+            namespace='plotting'
+        ),
+
+        node(
+            plot_Educational_Level_distribution,
+            inputs="preprocessed_data",
+            outputs="plot_Educational_Level_distribution_px",
+            name="plot_Educational_Level_distribution",
+            namespace='plotting'
         )
     ])
+
