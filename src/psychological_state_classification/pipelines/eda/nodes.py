ebfdef8706fc96e03eba734c94358638081eb7a1
Katarzyna Dybcio <kasiadybcio7@gmail.com>
2025-01-02 16:46:05 +0100

    plots eda

--- src/psychological_state_classification/pipelines/eda/nodes.py
+++ src/psychological_state_classification/pipelines/eda/nodes.py
@@ -2,3 +2,25 @@
 This is a boilerplate pipeline 'eda'
 generated using Kedro 0.19.10
 """
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_missing(data :pd.DataFrame):
+    sns.set_style("whitegrid",{'font_scale':'1.6'})
+    plt.figure(figsize=(10, 6))
+    plt.title("Missing data per column %")
+    sns.barplot(y = data.isna().sum()/data.shape[0],
+                x = data.columns)
+    plt.ylim(0,1)
+    plt.xticks(rotation=90)
+    return plt
+
+def plot_dist_hrv(data :pd.DataFrame):
+    sns.set_style("whitegrid",{'font_scale':'1.6'})
+    plt.figure(figsize=(10, 6))
+    sns.histplot(data['HRV'])
+    plt.title('Distribution of Heart Rate Variability')
+    plt.xlabel('HRV')
+    plt.ylabel('Count')
+    return plt
--- src/psychological_state_classification/pipelines/eda/pipeline.py
+++ src/psychological_state_classification/pipelines/eda/pipeline.py
@@ -3,8 +3,22 @@ This is a boilerplate pipeline 'eda'
 generated using Kedro 0.19.10
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import *
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=plot_missing,
+            inputs="raw_data",
+            outputs="missing_data_plot",
+            name="plot_missing_node"
+        ),
+        node(
+            func=plot_dist_hrv,
+            inputs="preprocessed_data",
+            outputs="dist_hrv_plot",
+            name="plot_hrv_distribution"
+        )
+    ])
