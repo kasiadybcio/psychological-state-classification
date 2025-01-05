47f8ceb9220bb7933997a48c39f33b43d173cf5f
Quoc Frejter <an.frejter@gmail.com>
2025-01-05 20:22:59 +0100

    Add feature engineering pipeline

---
+++ src/psychological_state_classification/pipelines/feature_engineering/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'feature_engineering'
+generated using Kedro 0.19.10
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
